d3c1ea325b82a5e2dafcc6ea6bdc1c71d2d89e3e
Divyang Dheer <ios.divyang@spaarksweb.com>
2025-02-03 14:40:46 +0530

    splashscree

--- src/screen/SplashScreen.js
+++ src/screen/SplashScreen.js
@@ -1,17 +1,44 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React, {useEffect} from 'react';
-import {resetAndNavigate} from '../utils/navigationUtils';
+import { Animated, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { resetAndNavigate } from '../utils/navigationUtils';
 
 const SplashScreen = () => {
+  const [isStop] = useState(false);
+  const scale = new Animated.Value(1);
+
   useEffect(() => {
     setTimeout(() => {
       resetAndNavigate('TabNavigator');
-    }, 1000);
+    }, 1500);
   });
 
+  useEffect(() => {
+    const animation = Animated.loop(
+      Animated.sequence([
+        Animated.timing(scale, {
+          toValue: 1.8,
+          duration: 800,
+          useNativeDriver: true,
+        }),
+        Animated.timing(scale, {
+          toValue: 1,
+          duration: 600,
+          useNativeDriver: true,
+        })
+      ])
+    )
+    if (!isStop) {
+      animation.start();
+    }
+
+    return () => animation.stop();
+  }, [isStop])
+
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+      <Animated.View style={{ transform: [{ scale }] }}>
+        <Text>SplashScreen</Text>
+      </Animated.View>
     </View>
   );
 };
