9ac2d901315d09aa2c5c6801a0c2113eda2ebff2
ddheer29 <divyangdheer11@gmail.com>
2025-02-07 23:07:19 +0530

    homescreen breaking news carousel added, refresh added, code improvement

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, RefreshControl, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, { FC, useEffect, useState } from 'react';
 import { apiBaseUrl } from '../utils/config';
 import { apiKey } from '../utils/ApiKey';
@@ -8,16 +8,22 @@ import BreakingNews from '../components/BreakingNews';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import NewsSection from '../components/NewsSection';
+import { colors, sizes } from '../utils/ThemeUtil';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { BookmarkSquareIcon } from 'react-native-heroicons/outline';
+import { navigate } from '../utils/navigationUtils';
+import { formatDate } from '../utils/helper';
 
 const HomeScreen: FC = () => {
   const [breakingNews, setBreakingNews] = useState<any[]>([]);
   const [recommandedNews, setRecommandedNews] = useState<any[]>([]);
-  const [breakingNewsLoading, setBreakingNewsLoading] = useState<Boolean>(false);
-  const [recommendedNewsLoading, setrecommendedNewsLoading] = useState<Boolean>(false);
+  const [breakingNewsLoading, setBreakingNewsLoading] = useState<boolean>(false);
+  const [recommendedNewsLoading, setrecommendedNewsLoading] = useState<boolean>(false);
+  const [urlList, setUrlList] = useState<string[]>([])
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+
 
   const getBreakingNews = async () => {
-    console.log('calling getBreakingNews');
     try {
       setBreakingNewsLoading(true);
       console.log(`${apiBaseUrl}/top-headlines?country=in&apiKey=${apiKey}`);
@@ -32,7 +38,6 @@ const HomeScreen: FC = () => {
   };
 
   const getRecommandedNews = async () => {
-    console.log('calling getRecommandedNews');
     try {
       setrecommendedNewsLoading(true);
       const res = await axios.get(
@@ -46,46 +51,195 @@ const HomeScreen: FC = () => {
     }
   };
 
+
+  const handleClick = (item: any) => {
+    navigate('NewsDetailsScreen', item)
+  }
+
+  const toggleBookmarkAndSave = (item: any, index: number) => {
+
+  }
+
+
+  // render funcitons
+  const renderHeader = () => {
+    return (
+      <>
+        <Header />
+        {
+          breakingNewsLoading ? (
+            <Loading />
+          ) : (
+            <View>
+              <MiniHeader label={'Breaking News'} />
+              <BreakingNews label={'Breaking News'} data={breakingNews} />
+            </View>
+          )
+        }
+        <MiniHeader
+          label='Recommended News'
+        />
+      </>
+    )
+  }
+
+  const renderItem = ({ item, index }) => {
+    return (
+      <TouchableOpacity
+        key={index}
+        style={{
+          marginBottom: 16,
+          marginHorizontal: 16,
+        }}
+        onPress={() => handleClick(item)}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'flex-start',
+            width: '100%',
+          }}>
+          <View
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              width: '20%',
+            }}
+          >
+            <Image
+              source={{
+                uri:
+                  item.urlToImage ||
+                  "https://images.unsplash.com/photo-1495020689067-958852a7765e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3c3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+              }}
+              style={{ width: hp(10), height: hp(10), borderRadius: 12 }}
+              resizeMode="cover"
+            />
+          </View>
+          <View
+            style={{
+              width: '70%',
+              paddingLeft: 20,
+              justifyContent: 'center',
+            }}
+          >
+            {/* Author */}
+            <Text
+              style={{
+                fontSize: RFValue(12),
+                color: colors.darkGray141,
+              }}
+            >
+              {item?.author?.length > 20
+                ? item.author.slice(0, 20) + "..."
+                : item.author}
+            </Text>
+
+            {/* Title */}
+            <Text
+              style={{
+                fontSize: hp(1.7),
+                fontWeight: 'bold',
+                color: colors.black,
+                textTransform: 'capitalize',
+                maxWidth: '90%',
+              }}
+            >
+              {item.title.length > 50
+                ? item.title.slice(0, 50) + "..."
+                : item.title}
+            </Text>
+
+            {/* Date */}
+            <Text
+              style={{
+                fontSize: RFValue(10),
+                color: colors.darkGray141,
+              }}
+            >
+              {formatDate(item.publishedAt)}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              width: '10%',
+              justifyContent: 'center',
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => toggleBookmarkAndSave(item, index)}
+            >
+              <BookmarkSquareIcon
+                color={"gray"}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
+  const renderEmptyComponent = () => {
+    return (
+      <>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: sizes.width * 0.4,
+          }}
+        >
+          <Text style={{
+            color: colors.darkgray,
+            fontSize: 20,
+            fontWeight: 'bold',
+            textAlign: 'center',
+            width: '80%',
+          }}>
+            Oops no news found
+          </Text>
+        </View>
+      </>
+    )
+  }
+
+  const fetchNews = async () => {
+    setRefreshing(true); // Start refreshing
+    await Promise.all([getBreakingNews(), getRecommandedNews()]);
+    setRefreshing(false); // Stop refreshing
+  };
+
   useEffect(() => {
-    getBreakingNews();
-    getRecommandedNews();
+    fetchNews()
   }, []);
+
+  useEffect(() => {
+    const urls = recommandedNews.map((item) => item.url)
+    setUrlList(urls);
+  }, [recommandedNews])
+
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
       <StatusBar />
-      <Header />
-      {
-        breakingNewsLoading ? (
-          <Loading />
-        ) : (
-          <View>
-            <MiniHeader label={'Breaking News'} />
-            {/* <BreakingNews label={'Breaking News'} data={breakingNews} /> */}
-          </View>
-        )
-      }
-      <View>
-        <MiniHeader
-          label='Recommended News'
-        />
-        <ScrollView
-          contentContainerStyle={{
-            paddingBottom: hp(80),
-          }}
-        >
-          {
-            recommendedNewsLoading ? (
-              <Loading />
-            ) : (
-              <NewsSection
-                label='Recommended News'
-                data={recommandedNews}
-              />
-            )
-          }
-        </ScrollView>
-      </View>
+      <FlatList
+        data={recommandedNews}
+        keyExtractor={(item, index) => index.toString()}
+        ListHeaderComponent={renderHeader}
+        renderItem={renderItem}
+        ListEmptyComponent={renderEmptyComponent}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={fetchNews}
+          // colors={['#ff6600', '#ff3300']} // Loader colors for Android
+          // tintColor={'#ff6600'} // Loader color for iOS
+          // progressBackgroundColor={'#fff'}
+          />
+        }
+      />
     </SafeAreaView>
   );
 };
